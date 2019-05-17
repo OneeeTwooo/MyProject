3f5c9591522fe778152828f6cdd2e8d7f9a441f6
OneeeTwooo <artsiom.vasileuski@gmail.com>
2019-05-17 13:58:11 +0300

    Update sql

--- common-JDBC/src/main/resources/migrations/V1_1__initial_script.sql
+++ common-JDBC/src/main/resources/migrations/V1_1__initial_script.sql
@@ -1,81 +1,119 @@
-CREATE TABLE `Damage_cost` (
-	`id_damage` int NOT NULL AUTO_INCREMENT,
-	`name` varchar(50) NOT NULL,
-	`cost` FLOAT(50) NOT NULL,
-	`is_deleted` varchar(10) NOT NULL,
-	`create_when` DATETIME NOT NULL,
-	`modify_when` DATETIME NOT NULL,
-	PRIMARY KEY (`id_damage`)
-);
+create table cars
+(
+  id_car int auto_increment
+    primary key,
+  gos_number varchar(10) not null,
+  mark varchar(20) not null,
+  model varchar(20) not null,
+  year_out date not null,
+  type varchar(4) not null,
+  full_cost float not null,
+  cost_1_day float not null,
+  is_deleted varchar(10) not null,
+  created_when datetime not null,
+  modify_when datetime not null,
+  constraint gos_number
+  unique (gos_number)
+)
+;
 
-CREATE TABLE `damage_rent` (
-	`id_damage_rent` int NOT NULL AUTO_INCREMENT,
-	`id_damage` int NOT NULL,
-	`id_rent` int NOT NULL,
-	PRIMARY KEY (`id_damage_rent`)
-);
+create table damage_cost
+(
+  id_damage int auto_increment
+    primary key,
+  name varchar(50) not null,
+  cost float not null,
+  is_deleted varchar(10) not null,
+  create_when datetime not null,
+  modify_when datetime not null
+)
+;
 
-CREATE TABLE `rents` (
-	`id_rent` int NOT NULL AUTO_INCREMENT,
-	`id_user` int NOT NULL,
-	`id_car` int NOT NULL,
-	`rental_start_date` DATETIME NOT NULL,
-	`rental_finish_date` DATETIME NOT NULL,
-	`created_when` DATETIME NOT NULL,
-	`modify_when` DATETIME NOT NULL,
-	PRIMARY KEY (`id_rent`)
-);
+create table roles
+(
+  id_role int auto_increment
+    primary key,
+  name_role varchar(45) not null,
+  type_role varchar(45) not null
+)
+;
 
-CREATE TABLE `cars` (
-	`id_car` int NOT NULL AUTO_INCREMENT,
-	`gos_number` varchar(10) NOT NULL UNIQUE,
-	`mark` varchar(20) NOT NULL,
-	`model` varchar(20) NOT NULL,
-	`year_out` DATE NOT NULL,
-	`type` varchar(4) NOT NULL,
-	`full_cost` FLOAT NOT NULL,
-	`cost_1_day` FLOAT NOT NULL,
-	`is_deleted` varchar(10) NOT NULL,
-	`created_when` DATETIME NOT NULL,
-	`modify_when` DATETIME NOT NULL,
-	PRIMARY KEY (`id_car`)
-);
+create table users
+(
+  id_user int auto_increment
+    primary key,
+  first_name varchar(45) not null,
+  last_name varchar(45) not null,
+  id_passport varchar(12) not null,
+  phone_number varchar(45) not null,
+  user_name varchar(45) not null,
+  user_password varchar(45) not null,
+  created_when datetime not null,
+  modify_when datetime not null,
+  is_deleted varchar(10) not null
+)
+;
 
-CREATE TABLE `role_names` (
-  `id_role` int(11) NOT NULL AUTO_INCREMENT,
-  `name_role` varchar(45) NOT NULL,
-  `type_role` varchar(45) NOT NULL,
-  PRIMARY KEY (`id_role`)
-);
+create table rents
+(
+  id_rent int auto_increment
+    primary key,
+  id_user int not null,
+  id_car int not null,
+  rental_start_date datetime not null,
+  rental_finish_date datetime not null,
+  created_when datetime not null,
+  modify_when datetime not null,
+  constraint rents_fk0
+  foreign key (id_user) references users (id_user),
+  constraint rents_fk1
+  foreign key (id_car) references cars (id_car)
+)
+;
 
-CREATE TABLE `user_role` (
-	`id_user_role` int NOT NULL AUTO_INCREMENT,
-	`id_role` int NOT NULL,
-	`id_user` int NOT NULL,
-	PRIMARY KEY (`id_user_role`)
-);
+create table damage_rent
+(
+  id_damage int not null,
+  id_rent int not null,
+  constraint damage_rent_fk0
+  foreign key (id_damage) references damage_cost (id_damage),
+  constraint damage_rent_fk1
+  foreign key (id_rent) references rents (id_rent)
+)
+;
 
-CREATE TABLE `users` (
-  `id_user` int(11) NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(45) NOT NULL,
-  `last_name` varchar(45) NOT NULL,
-  `id_passport` varchar(12) NOT NULL,
-  `phone_number` varchar(45) NOT NULL,
-  `user_name` varchar(45) NOT NULL,
-  `user_password` varchar(45) NOT NULL,
-  `created_when` datetime NOT NULL,
-  `modify_when` datetime NOT NULL,
-  PRIMARY KEY (`id_user`)
-);
+create index damage_rent_fk0
+  on damage_rent (id_damage)
+;
 
-ALTER TABLE `damage_rent` ADD CONSTRAINT `damage_rent_fk0` FOREIGN KEY (`id_damage`) REFERENCES `Damage_cost`(`id_damage`);
+create index damage_rent_fk1
+  on damage_rent (id_rent)
+;
 
-ALTER TABLE `damage_rent` ADD CONSTRAINT `damage_rent_fk1` FOREIGN KEY (`id_rent`) REFERENCES `rents`(`id_rent`);
+create index rents_fk0
+  on rents (id_user)
+;
 
-ALTER TABLE `rents` ADD CONSTRAINT `rents_fk0` FOREIGN KEY (`id_user`) REFERENCES `users`(`id_user`);
+create index rents_fk1
+  on rents (id_car)
+;
 
-ALTER TABLE `rents` ADD CONSTRAINT `rents_fk1` FOREIGN KEY (`id_car`) REFERENCES `cars`(`id_car`);
+create table user_role
+(
+  id_role int not null,
+  id_user int not null,
+  constraint user_role_fk0
+  foreign key (id_role) references roles (id_role),
+  constraint user_role_fk1
+  foreign key (id_user) references users (id_user)
+)
+;
 
-ALTER TABLE `user_role` ADD CONSTRAINT `user_role_fk0` FOREIGN KEY (`id_role`) REFERENCES roles(`id_role`);
+create index user_role_fk0
+  on user_role (id_role)
+;
 
-ALTER TABLE `user_role` ADD CONSTRAINT `user_role_fk1` FOREIGN KEY (`id_user`) REFERENCES `users`(`id_user`);
+create index user_role_fk1
+  on user_role (id_user)
+;
+
